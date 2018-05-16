5f9f8a9a29f4825c579debc94c5f02f835fdfe58
Niko_Zhang <593177876@qq.com>
2018-05-16 23:42:28 +0800

    配置了vue-loader

---
+++ webpack-vue/src/app.vue
@@ -0,0 +1,40 @@
+<template>
+    <!-- app.vue 的template标签内只能有一个节点 -->
+    <div id="app">
+        <span>vue</span>
+    </div>
+</template>
+
+<script>
+export default {
+  // 声明组件，之后便可以使用组件标签
+  components: {},
+  data(){
+      return {
+
+      }
+  }
+};
+</script>
+
+<!-- 设置scoped 表示当前组件下的id只在当前组件起作用，不会跟其他组件引起冲突 -->
+<style>
+#app {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+}
+
+#cover {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    background-color: #555;
+    opacity: 0.5;
+    z-index: -1;
+}
+</style>
