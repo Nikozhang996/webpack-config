bc8c7e003141f4c9b149881d56b156c1fde31dd2
Niko_Zhang <593177876@qq.com>
2018-05-17 09:56:52 +0800

    基础Vue配置完成

--- webpack-vue/src/app.vue
+++ webpack-vue/src/app.vue
@@ -1,7 +1,7 @@
 <template>
     <!-- app.vue 的template标签内只能有一个节点 -->
     <div id="app">
-        <span>vue</span>
+      
     </div>
 </template>
 
@@ -9,10 +9,8 @@
 export default {
   // 声明组件，之后便可以使用组件标签
   components: {},
-  data(){
-      return {
-
-      }
+  data() {
+    return {};
   }
 };
 </script>
@@ -20,21 +18,21 @@ export default {
 <!-- 设置scoped 表示当前组件下的id只在当前组件起作用，不会跟其他组件引起冲突 -->
 <style>
 #app {
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: 0;
-    bottom: 0;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
 }
 
 #cover {
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: 0;
-    bottom: 0;
-    background-color: #555;
-    opacity: 0.5;
-    z-index: -1;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  background-color: #555;
+  opacity: 0.5;
+  z-index: -1;
 }
 </style>
--- webpack-vue/webpack.config.js
+++ webpack-vue/webpack.config.js
@@ -1,52 +1,86 @@
 const path = require('path');
-// const HTMLPlugin = require('html-webpack-plugin');
-// const webpack = require('webpack');
-// const ExtractPlugin = require('extract-text-webpack-plugin');
+const HTMLPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const ExtractPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    target: 'web',    
-    entry: path.join(__dirname, 'src/index.js'), // 输入：项目主文件（入口文件）
-    output: { // 输出
-        filename: 'build.[hash:8].js', // 输出的文件名
-        path: path.join(__dirname, 'dist') // 输出路径
-    },
-    module: { // 配置加载资源
-        rules: [ // 规则
-            {
-                test: /\.vue$/,
-                use:[
-                    'vue-loader'
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.styl$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'stylus-loader'
-                ]
-            },
-            {
-                test: /\.jsx$/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.(gif|jpg|jpeg|png|svg)$/,
-                use: [{
-                    loader: 'url-loader',
-                    options: {
-                        limit: 1024, // 文件小于1024字节，转换成base64编码，写入文件里面
-                        name: '[name]-output.[ext]'
-                    }
-                }]
-            }
+const isDev = process.env.NODE_ENV === 'development'
+const config = {
+  target: 'web',
+  entry: path.join(__dirname, 'src/index.js'), // 输入：项目主文件（入口文件）
+  output: { // 输出
+    filename: 'build.[hash:8].js', // 输出的文件名
+    path: path.join(__dirname, 'dist') // 输出路径
+  },
+  module: { // 配置加载资源
+    rules: [ // 规则
+      {
+        test: /\.vue$/,
+        use: [
+          'vue-loader'
         ]
-    },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.styl$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'stylus-loader'
+        ]
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(gif|jpg|jpeg|png|svg)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 1024, // 文件小于1024字节，转换成base64编码，写入文件里面
+            name: '[name]-output.[ext]'
+          }
+        }]
+      }
+    ]
+  },
+  plugins: [
+    /* 
+    说明：用于区分开发环境还是生产环境，Vue或React框架会自动根据环境输出不同的文件类型
+     */
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: isDev ? '"development"' : '"production"'
+      }
+    }),
+    new HTMLPlugin({
+
+    })
+  ]
 }
+
+if (isDev) {
+  config.devtool = '#cheap-module-eval-source-map'
+  config.devServer = {
+    host: '0.0.0.0',
+    port: 3000,
+    hot: true,
+    overlay: {
+      errors: true
+    }
+  }
+  config.plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NormalModuleReplacementPlugin()
+  )
+} else {
+
+}
+
+module.exports = config;
