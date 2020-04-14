7b6f74cadc4b92f77efb4c221da61c17d2e4516a
Niko <593177876@qq.com>
2020-04-14 13:07:22 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './style/scss.scss'
 /* import Vue from "vue";
 import App from "./App.vue";
 
@@ -10,15 +11,12 @@ import * as ReactDOM from "react-dom";
 
 // import App from "./ReactApp/index.tsx";
 // ReactDOM.render(<App></App>, document.getElementById("root"));
-const div = document.getElementById（'#root')
-div.innerHTML = 'adf'
+const div = document.getElementById("root");
+div.className = "c1";
+div.innerHTML = "Hello";
 
-if (module.hot) {
-  module.hot.accept(); // 当入口文件变化后重新执行当前入口文件
-}
 
 /*
-import './style/scss.scss'
 
 import image from './images/img.png'
 
@@ -27,23 +25,11 @@ const img = new Image();
 img.src = image
 const div = document.getElementById('key')
 
-document.body.appendChild(img);
-
-
-function timer(value) {
-    return new Promise(function (resolve, reject) {
-        setTimeout(() => {
-            resolve(value ** 2)
-        }, 500);
-    })
-}
-
-async function func() {
-    const reuslt = await timer(8)
-    console.log(reuslt);
-}
-
-func() 
+document.body.appendChild(img); 
 */
 
 // @func
+
+if (module.hot) {
+  module.hot.accept(); // 当入口文件变化后重新执行当前入口文件
+}
