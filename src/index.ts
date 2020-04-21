cf13160a5066ad2a7c689c63a65e077b10433f06
NikoZhang <593177876@qq.com>
2020-04-21 10:17:14 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -6,23 +6,6 @@ console.log(min(3, 1));
 
 console.log("hello");
 
-/* import Vue from "vue";
-import App from "./App.vue";
-
-export default new Vue({
-  el: "#root",
-  render: (h: any) => h(App),
-}); 
-
-import React from "react";
-import ReactDOM from "react-dom";
-
-import App from "./ReactApp/index";
-ReactDOM.render(Reac.createElement(App), root);
-
-*/
-
 if (module.hot) {
   module.hot.accept(); // 当入口文件变化后重新执行当前入口文件
 }
-1;
